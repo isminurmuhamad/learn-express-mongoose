9592964c8c7ca436d14c96ef0b14459686c9bdad
Ismi Nur Muhamad <ismi.sthoz@gmail.com>
2024-02-15 00:22:04 +0700

    feat: update product

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const app = express();
 
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/shop_db')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // Route
 app.get('/', (req, res) => {
@@ -48,6 +50,12 @@ app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id)
     res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true })
+    res.redirect(`/products/${product._id}`)
 })
 
 app.listen(3000, () => {
